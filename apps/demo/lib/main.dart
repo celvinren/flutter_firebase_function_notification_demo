47b05aec98e6e8c52aada8b99fce0c55063e8d87
Calvin Ren <calvin.qut@gmail.com>
2023-06-05 19:11:14 +1000

    init fcm on app

--- apps/demo/lib/main.dart
+++ apps/demo/lib/main.dart
@@ -1,12 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:rxdart/rxdart.dart';
 import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+  String? token = await messaging.getToken();
+  print('User device token: $token');
   runApp(const MyApp());
 }
 
@@ -36,11 +54,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Stream<RemoteMessage?> getMessageStream() =>
+      _messageStreamController.asBroadcastStream();
+  final _messageStreamController = BehaviorSubject<RemoteMessage?>.seeded(
+    null,
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      _messageStreamController.add(message);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      _messageStreamController.add(message);
+    });
+
+    FirebaseMessaging.onBackgroundMessage((RemoteMessage message) async {
+      _messageStreamController.add(message);
     });
   }
 
@@ -55,20 +88,27 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            StreamBuilder(
+              stream: getMessageStream(),
+              builder: (ct, data) {
+                if (data.hasData) {
+                  final message = data.data;
+                  return Column(
+                    children: [
+                      Text(
+                        "Title: ${message?.notification?.title ?? ''}",
+                      ),
+                      Text(
+                        "Body: ${message?.notification?.body ?? ''}",
+                      ),
+                    ],
+                  );
+                }
+                return const SizedBox();
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
